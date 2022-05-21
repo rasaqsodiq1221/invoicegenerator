1091ae9e55ae3673cccfd90019fe0bb0679aa106
rasaqsodiq1221 <rasaqsodiq1221@gmail.com>
2022-05-21 16:45:46 +0100

    js basics

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// const is used often, let is only used when required
+const services = [
+    {
+        "service": "Wash Car",
+        "price": 10
+    }, {
+        "service": "Mow Lawn",
+        "price": 20
+    }, {
+        "service": "Pull Weeds",
+        "price": 30
+    }
+]
+
+const btnEl = document.getElementById("btn-el");
+let requiredService = []
+btnEl.addEventListener("click", function(){
+    requiredService.push(services[1])
+    console.log(requiredService)
+})
+
+
+// check on query selectorall... its output once you click on one
+
+
